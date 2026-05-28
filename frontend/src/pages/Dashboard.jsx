d3150791f1d5a0267891bb989dd80f15a8076404
Pratibha Aggarwal <pratibhaaggarwal32@gmail.com>
2026-05-28 21:39:50 +0530

    progress

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -19,8 +19,14 @@ function Dashboard() {
   const [expectedReturnTime, setExpectedReturnTime] = useState("")
 
   useEffect(() => {
+  fetchDevices()
+
+  const interval = setInterval(() => {
     fetchDevices()
-  }, [])
+  }, 3000)
+
+  return () => clearInterval(interval)
+    }, [])
 
   const fetchDevices = async () => {
     try {
@@ -108,6 +114,38 @@ function Dashboard() {
       <h1 className="text-3xl font-bold text-gray-800 mb-6">
         Device Dashboard
       </h1>
+
+
+      <p className="text-gray-500 mb-6">
+        Live device tracking system
+      </p>
+
+
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-8">
+        <div className="bg-white p-5 rounded-2xl shadow-sm">
+          <p className="text-gray-500">Available Devices</p>
+
+          <h2 className="text-3xl font-bold text-green-600 mt-2">
+           {
+            devices.filter(
+              (device) => device.status === "available"
+            ).length
+           } 
+          </h2>
+        </div>
+
+        <div className="bg-white p-5 rounded-2xl shadow-sm">
+         <p className="text-gray-500">Devices In Use</p>
+
+           <h2 className="text-3xl font-bold text-red-600 mt-2">
+            {
+              devices.filter(
+               (device) => device.status === "in-use"
+              ).length
+          }
+           </h2>
+        </div>
+      </div>
 
 
     <form
