e8526d0218af8c5feb930f9dd7c62523d24bc499
Pratibha Aggarwal <pratibhaaggarwal32@gmail.com>
2026-06-19 02:16:18 +0530

    request

--- frontend/src/services/requestService.js
+++ frontend/src/services/requestService.js
@@ -1,11 +1,9 @@
 import axios from "axios"
 
-const API_URL = "https://mydevicemanagement.onrender.com/api/requests"
+const API_URL = "http://localhost:5000/api/requests"
 
 const getConfig = () => {
-  const userInfo = JSON.parse(
-    localStorage.getItem("userInfo")
-  )
+  const userInfo = JSON.parse(localStorage.getItem("userInfo"))
   return {
     headers: {
       Authorization: `Bearer ${userInfo.token}`,
@@ -13,11 +11,37 @@ const getConfig = () => {
   }
 }
 
-// Employee: create a request for a device
+// Employee: request an available device (assignment)
 export const createRequest = async (deviceId) => {
+  const response = await axios.post(API_URL, { deviceId }, getConfig())
+  return response.data
+}
+
+// Employee: initiate a transfer to another employee
+export const createTransferRequest = async (deviceId, toEmployeeId) => {
   const response = await axios.post(
-    API_URL,
-    { deviceId },
+    `${API_URL}/transfer`,
+    { deviceId, toEmployeeId },
+    getConfig()
+  )
+  return response.data
+}
+
+// Employee: accept an incoming transfer
+export const acceptTransfer = async (requestId) => {
+  const response = await axios.put(
+    `${API_URL}/${requestId}/accept-transfer`,
+    {},
+    getConfig()
+  )
+  return response.data
+}
+
+// Employee: decline an incoming transfer
+export const declineTransfer = async (requestId) => {
+  const response = await axios.put(
+    `${API_URL}/${requestId}/decline-transfer`,
+    {},
     getConfig()
   )
   return response.data
@@ -25,23 +49,23 @@ export const createRequest = async (deviceId) => {
 
 // Employee: get their own requests
 export const getMyRequests = async () => {
-  const response = await axios.get(
-    `${API_URL}/my`,
-    getConfig()
-  )
+  const response = await axios.get(`${API_URL}/my`, getConfig())
   return response.data
 }
 
-// Admin: get all requests
+// Employee: get incoming transfer requests directed at them
+export const getIncomingTransfers = async () => {
+  const response = await axios.get(`${API_URL}/incoming-transfers`, getConfig())
+  return response.data
+}
+
+// Admin: get all assignment requests
 export const getRequests = async () => {
-  const response = await axios.get(
-    API_URL,
-    getConfig()
-  )
+  const response = await axios.get(API_URL, getConfig())
   return response.data
 }
 
-// Admin: approve a request
+// Admin: approve an assignment request
 export const approveRequest = async (requestId) => {
   const response = await axios.put(
     `${API_URL}/${requestId}/approve`,
@@ -51,7 +75,7 @@ export const approveRequest = async (requestId) => {
   return response.data
 }
 
-// Admin: reject a request
+// Admin: reject an assignment request
 export const rejectRequest = async (requestId) => {
   const response = await axios.put(
     `${API_URL}/${requestId}/reject`,
