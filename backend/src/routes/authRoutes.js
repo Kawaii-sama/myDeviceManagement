611b38d693928370a9d73f24a3d2ec90b2566c9e
Pratibha Aggarwal <pratibhaaggarwal32@gmail.com>
2026-06-19 02:21:54 +0530

    route again

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -4,21 +4,19 @@ const {
   createAdmin,
   loginUser,
   registerUser,
+  getAllUsers,
 } = require("../controllers/authController")
-
 
 const {
   protect,
   adminOnly,
 } = require("../middleware/authMiddleware")
 
-
 const router = express.Router()
 
 router.post("/create-admin", createAdmin)
-
 router.post("/login", loginUser)
-
 router.post("/register", registerUser)
+router.get("/users", protect, adminOnly, getAllUsers)
 
 module.exports = router
