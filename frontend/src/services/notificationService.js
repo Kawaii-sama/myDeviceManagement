aefb6479ad24a6304bdb488c5838c332464cdc74
Pratibha Aggarwal <pratibhaaggarwal32@gmail.com>
2026-06-19 14:59:07 +0530

    servicing

--- frontend/src/services/notificationService.js
+++ frontend/src/services/notificationService.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const API_URL =
-  "http://localhost:5000"
+  "http://localhost:5000/api/notifications"
 
 export const getNotifications = async () => {
   const userInfo = JSON.parse(
