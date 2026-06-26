1e9a7433f0123e7197bf9c34fb7469f850abb35c
Pratibha Aggarwal <pratibhaaggarwal32@gmail.com>
2026-06-26 16:00:58 +0530

    problem fixed

--- frontend/src/services/entryService.js
+++ frontend/src/services/entryService.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const API_URL = "http://localhost:5000/api/entries"
+const API_URL = `${import.meta.env.VITE_API_URL}/api/entries`
 
 const getConfig = () => {
   const userInfo = JSON.parse(localStorage.getItem("userInfo"))
